4e1fc2f7ec2394b1f08e82b9906829c11119b84f
ChevrollierG <chevrollier.guillaume@yahoo.com>
2022-07-22 22:37:40 +0200

    comment code and change names of images

--- kmeans.py
+++ kmeans.py
@@ -19,11 +19,21 @@ K=3
 
 
 def LoadImage(image):
+    """
+        Load the image with OpenCV
+        input: image: link to the image
+        output: 2 dimensions array containing RVB tuples
+    """
     url="C:/Users/guill/OneDrive/Bureau/Devoirs/Perso/Projets Info/Segmentation/pictures/"+image
     data=cv2.imread(url)
     return cv2.cvtColor(data,cv2.COLOR_BGR2RGB)
 
 def ToGray(data):
+    """
+        Turns an image to gray by averaging RVB chanels for each pixels
+        input: data: 2 dimensions list containing RVB tuples
+        output: 2 dimensions array containing integers
+    """
     matrice=[]
     for i in range(len(data)):
         matrice.append([])
@@ -32,6 +42,11 @@ def ToGray(data):
     return matrice
 
 def BigData(data):
+    """
+        Increase the dimensions of the array by 2
+        input: data: matrix
+        output: bigger matrix
+    """
     matrice=[]
     matrice.append(np.zeros(len(data[0])+2))
     for i in range(len(data)):
@@ -43,6 +58,12 @@ def BigData(data):
     return matrice
 
 def Edges(data,list_data):
+    """
+        Calculate edges for each pixels with the sobel operator and add it to list_data
+        input: data: matrix
+               list_data: a list of tuple representing each pixels by their parameters (dimensions, color, edges)
+        output: no output
+    """
     sobel_x=[[1,2,1],
              [0,0,0],
              [-1,-2,-1]]
@@ -59,6 +80,10 @@ def Edges(data,list_data):
             count+=1
       
 def ListPixel(data):
+    """
+        Make a list of pixels with their parameters (dimensions, color)
+        input: data: matrix
+    """
     list_pixel=[]
     for i in range(len(data)):
         for j in range(len(data[i])):
@@ -66,11 +91,21 @@ def ListPixel(data):
     return list_pixel
 
 def AfficheImage(data):
+    """
+        Plot the image with matplotlib
+        input: data: matrix
+    """
     plt.axis('off')
     plt.imshow(data)
     plt.show()
 
 def Distance(a,b):
+    """
+        Calculate the 'distance' or the error between two pixels
+        input: a: first pixel
+               b: second pixel
+        output: error
+    """
     resul=0
     for i in range(NUM_PARAM):
         resul+=((int(a[i])-int(b[i]))*WEIGHT[i])**2
@@ -78,8 +113,16 @@ def Distance(a,b):
     return resul
 
 def Init(k,data,size_x,size_y):
+    """
+        Choose k random centers (classes) to start the algorithm
+        input: k: parameter k
+               data: list of tuples representing pixels
+               size_x: horizontal length of the matrix
+               size_y: vertical length of the matrtix
+        output: a list of tuples representing the k pixels
+    """
     classes=[]
-    colors=[(255,0,0),(0,255,0),(0,0,255),(255,255,0),(255,0,255),(0,255,255),(255,255,255),(0,0,0)]
+    colors=[(255,0,0),(0,255,0),(0,0,255),(255,255,0),(255,0,255),(0,255,255),(0,0,0)]
     for i in range(k):
         test=False
         while not test:
@@ -97,12 +140,22 @@ def Init(k,data,size_x,size_y):
     return classes
     
 def Traitement(data,classes):
+    """
+        Loop over every items to calculate the distance with the classes and affect the color of the nearest class
+        input: data: list of tuple representing the pixels
+               classes: list of tuple representing the k pixel classes
+    """
     for item in data:
         if len(item)!=NUM_PARAM+1:
             classes=sorted(classes,key=lambda x: Distance(x,item))
             item.append(classes[0][len(classes[0])-1])
         
 def Segmentation(data):
+    """
+        Build a matrix of RVB tuple with the list of pixels
+        input: data: list of tuple representing the pixels
+        output: matrice: matrix colored by the classes
+    """
     matrice=[]
     for i in data:
         if i[0]==len(matrice):
@@ -111,6 +164,12 @@ def Segmentation(data):
     return matrice
 
 def UpdateCenter(data, classes):
+    """
+        Average dimension x and y of all cluster to update the center of each classes
+        input: data: list of tuples representing the pixels
+               classes: list of tuples representing the pixel classes
+        output: list of tuples representing the pixel classes updated
+    """
     new_classes=[]
     for i in classes:
         colored=list(filter(lambda n: n[NUM_PARAM]==i[NUM_PARAM], data))
@@ -121,7 +180,35 @@ def UpdateCenter(data, classes):
         new_classes.append(center[0])
     return new_classes
 
+def KmeansIteration(data, list_data, classes):
+    """
+        Loop until the center of each classes barely changes
+        input: data: matrix
+               list_data: list of tuples representing the pixels
+               classes: list of tuples representing the pixel classes
+        output: copy_data: the last iteration of the list of tuples representing pixels
+                copy_classes: the last iteration of the list of tuples representing pixel classes
+    """
+    test=True
+    while test:
+        copy_data=deepcopy(list_data)
+        Traitement(copy_data, classes)
+        copy_classes=classes
+        classes=UpdateCenter(copy_data, classes)
+        test=False
+        for i in range(len(classes)):
+            if abs(classes[i][0]-copy_classes[i][0])>=0.05*len(data) or abs(classes[i][1]-copy_classes[i][1])>=0.05*len(data[0]):
+                test=True
+                break
+    return copy_data, copy_classes
+
 def AfficheClasses(data, seg_data, classes):
+    """
+        Plot each classes on a different image 
+        input: data: matrix
+               seg_data: segmented matrix
+               classes: list of tuples representing pixels
+    """
     for c in classes:
         matrice=[]
         for i in range(len(seg_data)):
@@ -134,22 +221,12 @@ def AfficheClasses(data, seg_data, classes):
         AfficheImage(matrice)
 
 if __name__ =='__main__':
-    matrice=LoadImage("paysage6.jpg")
+    matrice=LoadImage("landscape7.jpg")
     AfficheImage(matrice)
     data=ListPixel(matrice)
     Edges(matrice, data)
     classes=Init(K, data, len(matrice), len(matrice[0]))
-    test=True
-    while test:
-        copy_data=deepcopy(data)
-        Traitement(copy_data, classes)
-        copy_classes=classes
-        classes=UpdateCenter(copy_data, classes)
-        test=False
-        for i in range(len(classes)):
-            if abs(classes[i][0]-copy_classes[i][0])>=0.05*len(matrice) or abs(classes[i][1]-copy_classes[i][1])>=0.05*len(matrice[0]):
-                test=True
-                break
+    copy_data, copy_classes=KmeansIteration(matrice, data, classes)
     new_matrice=Segmentation(copy_data)
     AfficheImage(new_matrice)
     AfficheClasses(matrice, new_matrice, copy_classes)
